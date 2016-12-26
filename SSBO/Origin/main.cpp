7a5f9c99ee3d19f767e68802e0a646e522d3c252
illidan.org <smile@illidan.org>
2016-12-26 08:55:06 +0800

    使用SSBO

---
+++ SSBO/Origin/main.cpp
@@ -0,0 +1,254 @@
+#include <stdio.h>
+#include <iostream>
+#include <windows.h>
+
+//#include <gl/GL.h>
+//#pragma comment(lib, "opengl32.lib")
+
+#include <GL/glew.h>
+#include <GL/wglew.h>
+#pragma comment(lib, "opengl32.lib")
+#pragma comment(lib, "glew32.lib")
+
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+
+#include "Misc.h"
+#include "Model.h"
+
+#include "Timer.h"
+#include "Frustum.h"
+
+//初始化参数
+int width = 1280;
+int height = 800;
+
+float identity[] = {
+	1, 0, 0, 0,
+	0, 1, 0, 0,
+	0, 0, 1, 0,
+	0, 0, 0, 1,
+};
+
+glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, -0.5f, -2.0f)) * glm::rotate(glm::mat4(1.0f), -90.0f, glm::vec3(0.0f, 1.0f, 0.0f)) * glm::scale(glm::mat4(1.0f), glm::vec3(0.01f, 0.01f, 0.01f));
+glm::mat4 projection = glm::perspective(45.0f, (float)width / (float)height, 0.1f, 1000.0f);
+
+HGLRC CreateMSAARC(HDC hdc)
+{
+	HGLRC rc;
+
+	GLint attribs [] = 
+	{
+		WGL_DRAW_TO_WINDOW_ARB, GL_TRUE,
+		WGL_ACCELERATION_ARB, WGL_FULL_ACCELERATION_ARB,
+		WGL_DOUBLE_BUFFER_ARB, GL_TRUE,
+		WGL_RED_BITS_ARB, 8,
+		WGL_GREEN_BITS_ARB, 8,
+		WGL_BLUE_BITS_ARB, 8,
+		WGL_ALPHA_BITS_ARB, 8,
+		WGL_DEPTH_BITS_ARB, 24,
+		WGL_STENCIL_BITS_ARB, 8,
+		WGL_SAMPLE_BUFFERS_ARB, GL_TRUE,
+		WGL_SAMPLES_ARB, 16,
+		NULL, NULL
+	};
+
+	int format[256] = {0};
+	UINT formatSize = 0;
+
+	wglChoosePixelFormatARB(hdc, attribs, nullptr, 256, format, &formatSize);
+	if (formatSize > 0)
+	{
+		PIXELFORMATDESCRIPTOR pfd;
+		DescribePixelFormat(hdc, format[0], sizeof(pfd), &pfd);
+		SetPixelFormat(hdc, format[0], &pfd);
+
+		int contexAttribs [] =
+		{
+			WGL_CONTEXT_MAJOR_VERSION_ARB, 4,
+			WGL_CONTEXT_MINOR_VERSION_ARB, 3, 
+			WGL_CONTEXT_PROFILE_MASK_ARB, WGL_CONTEXT_COMPATIBILITY_PROFILE_BIT_ARB,
+			NULL
+		};
+		rc = wglCreateContextAttribsARB(hdc, nullptr, contexAttribs);
+	}
+	
+	return rc;
+}
+
+LRESULT CALLBACK WinPro(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_CLOSE:
+		PostQuitMessage(0);
+		break;
+	case WM_DESTROY:
+		//PostQuitMessage(0);
+		break;
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+int __stdcall WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
+{
+	//注册窗口类
+	WNDCLASSEX wc;
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = WinPro;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = 0;
+	wc.hCursor = LoadCursor(hInstance, IDC_ARROW);
+	wc.hbrBackground = 0;
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = L"OpenGL";
+	wc.hIconSm = 0;
+	DWORD res = RegisterClassEx(&wc);
+	if (res == 0)
+	{
+		DWORD err = GetLastError();
+	}
+
+	//计算窗口大小和位置
+	int screenW = GetSystemMetrics(SM_CXSCREEN);
+	int screenH = GetSystemMetrics(SM_CYSCREEN);
+	RECT rect
+	{
+		(screenW - width) / 2,
+		(screenH - height) / 2,
+		rect.left + width,
+		rect.top + height
+	};
+
+	AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, FALSE);
+	//创建窗口并显示
+	HWND hWnd = CreateWindowEx(NULL, wc.lpszClassName, L"Demo", WS_OVERLAPPEDWINDOW, rect.left, rect.top, rect.right - rect.left, rect.bottom - rect.top, NULL, NULL, hInstance, NULL);
+	if (hWnd == 0)
+	{
+		DWORD err = GetLastError();
+	}
+
+	//创建OpenGL环境
+	HDC dc = GetDC(hWnd);
+	PIXELFORMATDESCRIPTOR pfd;
+	memset(&pfd, 0, sizeof(PIXELFORMATDESCRIPTOR));
+	pfd.nVersion = 1;
+	pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+	pfd.iLayerType = PFD_MAIN_PLANE;
+	pfd.iPixelType = PFD_TYPE_RGBA;
+	pfd.cColorBits = 32;
+	pfd.cDepthBits = 24;
+	pfd.cStencilBits = 8;
+	int pixelformat = ChoosePixelFormat(dc, &pfd);
+	SetPixelFormat(dc, pixelformat, &pfd);
+	HGLRC rc = wglCreateContext(dc);
+	wglMakeCurrent(dc, rc);
+
+	//初始化Glew
+	glewInit();
+
+	//开启MSAA,多重采样抗锯齿
+	if (wglChoosePixelFormatARB)
+	{
+		wglMakeCurrent(dc, nullptr);
+		wglDeleteContext(rc);
+		rc = nullptr;
+		ReleaseDC(hWnd, dc);
+		dc = nullptr;
+		DestroyWindow(hWnd);
+		hWnd = CreateWindowEx(NULL, wc.lpszClassName, L"Demo", WS_OVERLAPPEDWINDOW, rect.left, rect.top, rect.right - rect.left, rect.bottom - rect.top, NULL, NULL, hInstance, NULL);
+		dc = GetDC(hWnd);
+		rc = CreateMSAARC(dc);
+		wglMakeCurrent(dc, rc);
+	}
+
+/************************************************************************/
+/*	Obj                                                                 */
+
+	//编译链接GPU Program
+	GLuint program = CreateGPUProgram("../res/shader/Simple.vs", "../res/shader/Simple.fs");
+
+	//计算参数位置
+	GLuint MLocation, VLocation, PLocation;
+
+	MLocation = glGetUniformLocation(program, "M");
+	VLocation = glGetUniformLocation(program, "V");
+	PLocation = glGetUniformLocation(program, "P");
+	
+	GLuint textureLocation;
+	textureLocation = glGetUniformLocation(program, "U_MainTexture");
+
+	//加载Obj文件
+	Vertex* pVertex;
+	unsigned int vertexSize;
+	unsigned int* pIndex;
+	unsigned int indexSize;
+
+	LoadObjModel("../res/model/niutou.obj", &pVertex, vertexSize, &pIndex, indexSize);
+
+	//创建缓冲区对象
+	GLuint ssbo = CreateGLBufferObject(GL_SHADER_STORAGE_BUFFER, sizeof(Vertex) * vertexSize, GL_STATIC_DRAW, pVertex);
+	GLuint ibo = CreateGLBufferObject(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * indexSize, GL_STATIC_DRAW, pIndex);
+
+	//加载纹理
+	GLuint mainTexture = CreateTextureFromFile("../res/image/niutou.bmp");
+
+	//定义绘制Lambert表达式
+	auto DrawSomething = [&]()->void
+	{
+		//调用GPU Program
+		glUseProgram(program);
+		glUniformMatrix4fv(MLocation, 1, GL_FALSE, glm::value_ptr(model));
+		glUniformMatrix4fv(VLocation, 1, GL_FALSE, identity);
+		glUniformMatrix4fv(PLocation, 1, GL_FALSE, glm::value_ptr(projection));
+
+		glBindTexture(GL_TEXTURE_2D, mainTexture);
+		glUniform1i(textureLocation, 0);
+
+		glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, ssbo);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+		glDrawElements(GL_TRIANGLES, indexSize, GL_UNSIGNED_INT, 0);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+		
+		glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, 0);
+
+		glUseProgram(0);
+	};
+/************************************************************************/
+
+	glEnable(GL_DEPTH_TEST);
+
+	//显示和更新窗口
+	ShowWindow(hWnd, SW_SHOW);
+	UpdateWindow(hWnd);
+
+	//消息循环
+	MSG msg;
+	while (true)
+	{
+		if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT)
+			{
+				//GLenum error = glGetError();
+				break;
+			}
+
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+		glClearColor(0.4f, 0.3f, 0.2f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		DrawSomething();
+		  
+		glFlush();
+		SwapBuffers(dc);
+	}
+
+	glDeleteProgram(program);
+	return 0;
+}
